5cdc9898ee61ae52fb360a187206044ac33b408f
Piotr Adamus <adamusptr@gmail.com>
2017-03-22 21:07:53 +0100

    Aktualizacja funkcji przeglądania oferty.

--- enlarge.js
+++ enlarge.js
@@ -1,54 +1,78 @@
-function enlarge(imgName){
+// zmienna galleryArray zwraca indeksy wszystkich obrazków;
+var galleryArray = document.getElementsByClassName("galeria")
 
-// Get the modal
+// zmienna descriptionArray zwraca indeksy wszystkich opisów;
+var descriptionArray = document.getElementsByClassName("gallerydescription")
 
+// zmienna modalCon posłuży do przywrócenia przez funkcję display: block dla naszego modala;
+var modalCon = document.getElementById("modalContainer");
 
-var modal = document.getElementById('myModal');
+// zmienna modalImg posłuży do nadania obrazkowi modala nowego źródła;
+var modalImg = document.getElementById("modalImage");
 
-modal.style.display = "block";
+// zmienna modalCaption posłuży do nadania obrazkowi opisu modala nowego źródła;
+var modalCaption = document.getElementById("modalcaption");
 
-modalImg = document.getElementById("img01");
-modalImg.src = imgName;
+// zmienne dla guzików posłużą nam do zamykania i przewijania modala;
+var modalCloseButton = document.getElementById("modalclosebutton");
+var modalLeftButton = document.getElementById("modalleftbutton");
+var modalRightButton = document.getElementById("modalrightbutton");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+$(".galeria").click(function() {
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
-}
-}
+// nadaje wciśniętemu obrazkowi klasę activemodal
+    $(this).addClass('activemodal');
 
+// tworzy zmienną activeImageIndex, która zawiera indeks wciśniętego obrazka
+    for (i = 0; i < galleryArray.length; i++) {
+      if (galleryArray[i].classList.contains("activemodal")){
+        var activeImageIndex = i;
+      }
+    }
 
-var slideIndex = 1;	
+    $(descriptionArray[activeImageIndex]).addClass('activedescription');
 
-showModal(slideIndex);
+// nadaje naszemu bodalowi display: block;
+    modalCon.style.display = "block";
 
-function plusmodal(n) {
-  showModal(slideIndex += n);													
-}
+// zmienia adres obrazka z modala na adres wciśniętego zdjęcia;
+    modalImg.src = galleryArray[activeImageIndex].src;
+    modalCaption.textContent = descriptionArray[activeImageIndex].textContent;
 
-function showModal(n) {
-  var i;
-  var x		= document.getElementsByClassName("galeria");							
+// nadaje krzyżykowi funkcję zamykania modala;
+    modalCloseButton.onclick = function() {
+      modalCon.style.display = "none";
+      $(galleryArray[activeImageIndex]).removeClass('activemodal');
+      $(descriptionArray[activeImageIndex]).removeClass('activedescription');
+    }
 
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  
-  
+// nadaje guzikowi w lewo funkcjonalność;
+    modalLeftButton.onclick = function() {
+      modalImg.src = galleryArray[activeImageIndex-1].src;
+      $(galleryArray[activeImageIndex-1]).addClass('activemodal');
+      $(galleryArray[activeImageIndex]).removeClass('activemodal');
+      modalCaption.textContent = descriptionArray[activeImageIndex-1].textContent;
+      $(descriptionArray[activeImageIndex-1]).addClass('activedescription');
+      $(descriptionArray[activeImageIndex]).removeClass('activedescription');
+      activeImageIndex = activeImageIndex - 1;
+      $("#modalImage").finish();
+      $("#modalImage").css({ opacity: '0', blur: '10px' });
+      $("#modalImage").animate({opacity: '1', blur: '0px'}, "slow");
 
-//   $("#img01").animate({
- //       opacity: '0',
-   // });
-	
-    for (i = 0; i < x.length; i++) {
-	 modalImg.src = x[slideIndex-1].src
-  }
-   
+    }
 
-  
-// $("#img01").animate({
-  //      opacity: '1',
-    //});
-  
-}
+// nadaje guzikowi w prawo funkcjonalność;
+    modalRightButton.onclick = function() {
+      modalImg.src = galleryArray[activeImageIndex+1].src
+      $(galleryArray[activeImageIndex+1]).addClass('activemodal');
+      $(galleryArray[activeImageIndex]).removeClass('activemodal');
+      modalCaption.textContent = descriptionArray[activeImageIndex+1].textContent
+      $(descriptionArray[activeImageIndex+1]).addClass('activedescription');
+      $(descriptionArray[activeImageIndex]).removeClass('activedescription');
+      activeImageIndex = activeImageIndex + 1;
+      $("#modalImage").finish();
+      $("#modalImage").css({ opacity: '0', blur: '10px' });
+      $("#modalImage").animate({ opacity: '1', blur: '0px'}, "slow");
+    }
+
+});
